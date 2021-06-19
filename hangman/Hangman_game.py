8242d527e2630a940721a31508d4888afe0c92ff
mudit <mudit.shukla_cs19@gla.ac.in>
2021-06-19 17:47:48 +0530

    Hangman game updated

--- hangman/Hangman_game.py
+++ hangman/Hangman_game.py
@@ -1,6 +1,9 @@
 import random
+from hangman.hangman_stages_logo import stages, logo
+from hangman.words import word_list
 
-word_list = ["aardvark", "baboon", "camel"]
+print(logo)
+
 chosen_word = random.choice(word_list)
 display = ["_"]*len(chosen_word)
 number_of_lives = 6
@@ -8,62 +11,6 @@ print(display)
 game_over = False
 
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
 def fill_character(ch):
     global number_of_lives, game_over
@@ -80,6 +27,7 @@ def fill_character(ch):
             game_over = True
             print(stages[number_of_lives])
             print("You lose")
+            print(f"The word was {chosen_word}")
         else:
             print(stages[number_of_lives])
     print(display)
