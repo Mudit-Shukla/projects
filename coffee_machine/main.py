b0fd8f3e41651a5f88d672595deaa0a3f433d6f7
mudit <mudit.shukla_cs19@gla.ac.in>
2021-06-21 15:37:07 +0530

    Coffee machine game completed successfully

---
+++ coffee_machine/main.py
@@ -0,0 +1,38 @@
+from coffee_machine.menus import MENU, resources
+
+machine_on = True
+profit = 0
+
+def check_resource_availability(choice):
+    for item in resources:
+        if resources[item] < MENU[choice]["ingredients"][item]:
+            print(f"Sorry {choice} is not available at the moment")
+            return False
+    return True
+
+def make_payment(choice):
+    amount_made = float(input(f"Please Deposit {MENU[choice]['cost']}"))
+    amount_made = round(amount_made, 2)
+    if amount_made >= MENU[choice]['cost']:
+        print(f"You have deposited Rs {amount_made} and Rs {amount_made - MENU[choice]['cost']} has been refunded")
+        print(f"Enjoy your {choice}")
+        return True
+    else:
+        print(f"You have deposited Rs {amount_made} less than {MENU[choice]['cost']}. The amount has been refunded")
+        return False
+
+
+
+while machine_on:
+    choice =  input("What would you like? (tea/coffee/cappuccino):").upper()
+    if choice == "OFF":
+        machine_on = False
+    elif choice == "REPORT":
+        print(resources)
+        print(f"Money : {profit}")
+    elif choice == "TEA" or choice == "COFFEE" or choice == "CAPPUCCINO":
+        result = check_resource_availability(choice)
+        if result == True:
+            make_payment(choice)
+    else:
+        print("Sorry, we do not have your request in our menu")
---
+++ coffee_machine/menus.py
@@ -0,0 +1,32 @@
+MENU = {
+    "TEA": {
+        "ingredients": {
+            "water": 50,
+            "milk": 40,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "COFFEE": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "CAPPUCCINO": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
